d70275077719ccc43d8268a8731cdc9e37dd69ca
IsraelAbreu <israeloliveira18@hottmail.com>
2025-10-25 00:21:38 -0300

    Definido o Layout das view

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
-</div>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/js/bootstrap.js') }}">
+    <title>Todoist</title>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,55 +1,44 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/js/bootstrap.js') }}">
-    <title>Todoist</title>
-</head>
-
-<body>
+@extends('layout.layout')
+@section('content')
     <div class="container">
-        @yield('content')
-        <div class="div">
-            <h2 class="text-left" id="h2-logo">todoist</h2>
+    @yield('content')
+    <div class="div">
+        <h2 class="text-left" id="h2-logo">todoist</h2>
+    </div>
+    <div class="mt-5">
+        <div class="">
+            <h2>Login</h2>
         </div>
-        <div class="mt-5">
-            <div class="">
-                <h2>Login</h2>
-            </div>
-            <div class="mt-5" style="width: 30%">
-                <form action="{{ route('loginSubmit') }}" method="GET">
-                    @csrf
-                    <div class="mb-3">
-                        <label for="inputEmail" class="form-label">Email</label>
-                        <input type="text" class="form-control" id="inputEmail" name="email" value="{{ old('email') }}">
-                        @error('email')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label for="inputPassword" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="inputPassword" name="password" value="{{ old('password') }}">
-                         @error('password')
-                            <div class="text-danger">{{ $message }}</div>
-                        @enderror
-                    </div>
-                    <button type="submit" class="btn" id="btn-login">Login</button>
-                </form>
-                <div class="mt-1">
-                    @if (session('loginError'))
-                        <div class="alert alert-danger text-center">
-                            {{ session('loginError') }}
-                        </div>
-                    @endif
+        <div class="mt-5" style="width: 30%">
+            <form action="{{ route('loginSubmit') }}" method="GET">
+                @csrf
+                <div class="mb-3">
+                    <label for="inputEmail" class="form-label">Email</label>
+                    <input type="text" class="form-control" id="inputEmail" name="email"
+                        value="{{ old('email') }}">
+                    @error('email')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
                 </div>
+                <div class="mb-3">
+                    <label for="inputPassword" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" name="password"
+                        value="{{ old('password') }}">
+                    @error('password')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+                <button type="submit" class="btn" id="btn-login">Login</button>
+            </form>
+            <div class="mt-1">
+                @if (session('loginError'))
+                    <div class="alert alert-danger text-center">
+                        {{ session('loginError') }}
+                    </div>
+                @endif
             </div>
         </div>
     </div>
-</body>
+</div>
 
-</html>
+@endsection
